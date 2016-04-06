8d1eaf3294a6aede68836b3474f69821b8b167ce
MrMendonez <dmendonez@gmail.com>
2016-04-05 21:58:37 -0400

    init commit

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+// Scripts for Trivia Game
+
+$(document).ready(function() {
+    
+  // User can type name immediately on page load
+  $('#player-name').focus();
+
+  // Hide Questions
+  $('.questions').hide();
+
+  function removeWelcomeScreen() {
+    $('#welcome-screen').detach();
+    $('#username').text(playerName);
+    $('#question1').show().addClass('animated fadeIn');
+    $('#answer1').focus();
+  };
+
+  // Store playername and begin game
+  $('#name-button').on('click', function(e) {
+    e.preventDefault();
+    playerName = $('#player-name').val().trim() || 'Player';
+    $('#welcome-screen').addClass('animated slideOutUp');
+    setTimeout(removeWelcomeScreen, 1000);
+  });
+
+  var playerAnswer = '';
+  var correctAnswer = '';
+
+  function answerCheck() {
+    if(playerAnswer === correctAnswer) {
+      // do something
+      console.log('Correct!')
+    }
+    else {
+      // do something else
+      console.log('Incorrect!');
+    }
+  }
+
+  // Answer1 Button Actions
+  $('#answer1-btn').on('click', function(e) {
+    console.log('answer1-btn click function fired');
+    e.preventDefault();
+    correctAnswer = 'Jafar'
+    playerAnswer = $('#answer1').val().trim();
+    answerCheck();
+  })
+
+
+}); // end 
